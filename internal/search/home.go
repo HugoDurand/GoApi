c1eb289d911dce6d0359520b86906890a3b05bd0
HugoDurand <hugosmails94@gmail.com>
2020-05-15 16:41:55 +0200

    call with goroutines in channel

--- internal/search/home.go
+++ internal/search/home.go
@@ -4,37 +4,76 @@ import(
 	"net/http"
 	"log"
 	"io/ioutil"
+	"time"
 )
 
-func getBehanceContent()([]byte){
-	//https://stats.nba.com/stats/boxscore
-	res, err := http.Get("https://api.carbonintensity.org.uk/intensity")
+func getNotWorkingFirstContent(c chan []byte){
+	client := http.Client{
+		Timeout: 3 * time.Second,
+	}
+	res, err := client.Get("https://stats.nba.com/stats/boxscore")
 	if err != nil {
-		log.Fatal("Something went wrong getting Behance data")
+		close(c)
+		log.Print("Something went wrong getting other data")
+		return
 	}
 	defer res.Body.Close()
 	contents, err := ioutil.ReadAll(res.Body)
-	print(contents)
+	print("First data")
         if err != nil {
-            log.Fatal("Something went wrong getting Behance data")
+            close(c)
+			log.Print("Something went wrong reading other data")
+			return
         }
-	return contents
+	c <- contents
 }
 
-type Response struct {
-	Content []string
+func getSecondContent(c chan []byte){
+	res, err := http.Get("https://restcountries.eu/rest/v2/all")
+	if err != nil {
+		close(c)
+		log.Print("Something went wrong getting other data")
+		return
+	}
+	defer res.Body.Close()
+	contents, err := ioutil.ReadAll(res.Body)
+	print("Second data")
+        if err != nil {
+            close(c)
+			log.Print("Something went wrong reading other data")
+			return
+        }
+	c <- contents
+}
+
+func getThirdContent(c chan []byte){
+	res, err := http.Get("https://api.carbonintensity.org.uk/intensity")
+	if err != nil {
+		close(c)
+		log.Print("Something went wrong getting Behance data")
+		return
+	}
+	defer res.Body.Close()
+	contents, err := ioutil.ReadAll(res.Body)
+	print("Third data")
+        if err != nil {
+			close(c)
+			log.Print("Something went wrong reading Behance data")
+			return
+        }
+	c <- contents
 }
 
 func ResponseHandler(w http.ResponseWriter, r *http.Request){
-	//queryparams := r.URL.Query()["tags"]
-	//responseContent := Response{queryparams}
-	res:=getBehanceContent()
-	//resp, error := json.Marshal(res)
-	//if error != nil {
-	//	http.Error(w, error.Error(), http.StatusInternalServerError)
-	//	return
-	//}
+
+	c:= make(chan []byte)
+	go getNotWorkingFirstContent(c)
+	go getSecondContent(c)
+	go getThirdContent(c)
+	res, res2, res3 := <-c, <-c, <-c
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
+	w.Write(res2)
+	w.Write(res3)
 }
